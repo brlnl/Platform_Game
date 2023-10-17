da3999f61099e8e914e2ab276c7ec008a0b7adae
brlnl <unalberil5@gmail.com>
2023-10-17 21:49:41 +0200

    repaint() removed

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -20,24 +20,25 @@ private int xDelta = 100, yDelta = 100;
 
     public void changeXDelta(int value){
         this.xDelta += value;
-        repaint();
+        
     }
 
     public void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
+        
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+        
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         g.fillRect(xDelta , yDelta, 200, 50);
+        repaint();
     }
     
 }
