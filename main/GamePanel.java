21ac2e836302953a79dcd0fa82f4b19c67dd81ff
brlnl <unalberil5@gmail.com>
2023-10-19 22:11:51 +0200

    FPS

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -3,14 +3,23 @@ package main;
 import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
 private MouseInputs mouseInputs;
 private int xDelta = 100, yDelta = 100;
+private float xDir = 0.003f, yDir = 0.003f;
+private int frames = 0;
+private long lastCheck = 0;
+private Color color = new Color(150, 20, 90);
+private Random random;
 
     public GamePanel(){
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener( mouseInputs);
@@ -36,11 +45,42 @@ private int xDelta = 100, yDelta = 100;
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        updateRectangle();
+        g.setColor(color);
 
-        g.fillRect(xDelta , yDelta, 200, 50);
-        //frames++;
-        //if(System.currentTimeMillis() - las)
+        g.fillRect((int)xDelta , (int)yDelta, 200, 50);
+        frames++;
+
+        if(System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
         repaint();
     }
     
+    private void updateRectangle(){
+        xDelta+= xDir;
+        if(xDelta > 400 || xDelta < 0){
+            xDir *= -1;
+            color =  getRndColor();
+        }
+            
+
+        yDelta+= yDir;
+        if(yDelta > 400 || yDelta < 0){
+            yDir *= -1;
+            color = getRndColor();
+        }
+            
+        
+       
+            
+    } private Color getRndColor(){
+            int r = random.nextInt(255);
+            int b = random.nextInt(255);
+            int g = random.nextInt(255);
+
+            return new Color(r,g,b);
+        }
 }
